72c169bf9963d9588e55bd9394487545370a09a4
8t104k <8t104k@gmail.com>
2023-10-28 18:35:30 +0900

    Update random.rb どの人数でも、どのグループ数でも分けられるように対応

--- group/random.rb
+++ group/random.rb
@@ -1,22 +1,34 @@
 
-def num_of_people_in_group(all_people_num)
-  key = rand(2..all_people_num-2)
-  return [key,all_people_num - key]
+def num_of_people_in_group(people_num, group_num)
+  min_num = 2 #最小の人数
+  max_num = people_num - (group_num - 1)*min_num
+  new_nums = []
+
+  new_nums << rand(min_num..max_num) #最小値と最大値の間でランダムな数をとる
+  left_num = people_num - new_nums[0] #残りの人数
+
+  if group_num - 1 == 1
+    new_nums << left_num
+  else
+    #残った人数で残りのグループを分ける
+    new_nums.concat(num_of_people_in_group(left_num,group_num - 1))
+  end
+
+  return new_nums
 end
 
 def separete_group(group_num_array,all_people_array)
-  g1_num,g2_num = group_num_array
-  g1 = []
-  g2 = []
-
-  g1 = all_people_array.sample(g1_num)
-  p(g1.sort)
-  g2 = all_people_array - g1
-  p(g2)
-
+  new_groups = []
+  group_num_array.map do |num|
+    new_groups << all_people_array.sample(num).sort
+    all_people_array = all_people_array - new_groups[-1]
+  end
+  return new_groups
 end
 
 peoples = ['A','B','C','D','E','F']
 
-g_nums = num_of_people_in_group(peoples.length)
-separete_group(g_nums,peoples)
+g_nums = num_of_people_in_group(peoples.length,2)
+separete_group(g_nums,peoples).map do |group|
+  p(group)
+end
