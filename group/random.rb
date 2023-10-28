5afe366e74b01932f099555c4de9d54309306758
8t104k <8t104k@gmail.com>
2023-10-28 18:46:19 +0900

    コメントを追加

--- group/random.rb
+++ group/random.rb
@@ -21,7 +21,9 @@ end
 def separete_group(group_num_array,all_people_array)
   new_groups = []
   group_num_array.map do |num|
+    #人数分の要素を抽出して、新しい配列に追加
     new_groups << all_people_array.sample(num).sort
+    #元の配列から、抽出済みの要素を削除する
     all_people_array = all_people_array - new_groups[-1]
   end
   return new_groups
