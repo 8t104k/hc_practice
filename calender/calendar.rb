c2a47a827dbd7b89f96139dc5560800d0c5c0302
8t104k <8t104k@gmail.com>
2023-10-29 21:52:42 +0900

    日付の出力を追加

--- calender/calendar.rb
+++ calender/calendar.rb
@@ -4,11 +4,13 @@ s_date = Date.new(2023,10,1)
 e_date = Date.new(2023,10,-1)
 month = "    #{s_date.strftime('%B')} #{s_date.strftime('%Y')}"
 days_arry = [
-  ['Su','Mo','Tu','We','Th','Fr','Sa'],
+  ['Mo','Tu','We','Th','Fr','Sa','Su'],
   (1..31).to_a
   ]
 
 printf(month)
+print("\n")
 days_arry.map do |i|
-  print(i)
+  i.map { |l| print(l.to_s + " ") }
+  print("\n")
 end
