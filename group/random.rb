b48fd910e0cf49fe69c71127df92b8fed54b7e3e
8t104k <8t104k@gmail.com>
2023-10-29 21:56:48 +0900

    timesメソッドをuptoメソッドに変更

--- group/random.rb
+++ group/random.rb
@@ -1,37 +1,27 @@
-
-def num_of_people_in_group(people_num, group_num)
+def separete_group(all_people_array,group_num)
   min_num = 2 #最小の人数
-  max_num = people_num - (group_num - 1)*min_num
-  new_nums = []
-
-  new_nums << rand(min_num..max_num) #最小値と最大値の間でランダムな数をとる
-  left_num = people_num - new_nums[0] #残りの人数
-
-  if group_num - 1 == 1
-    #残りが一つのグループならば、残りの人数を全て割り当てる
-    new_nums << left_num
-  else
-    #残った人数でさらにグループを分ける
-    new_nums.concat(num_of_people_in_group(left_num,group_num - 1))
-  end
-
-  return new_nums
-end
-
-def separete_group(group_num_array,all_people_array)
   new_groups = []
-  group_num_array.map do |num|
+
+  #グループの数だけ繰り返す
+  1.upto(group_num) {|n|
+  if n != group_num
+    max_num = all_people_array.length - (group_num - n)*min_num
     #人数分の要素を抽出して、新しい配列に追加
-    new_groups << all_people_array.sample(num).sort
+    new_groups << all_people_array.sample(rand(min_num..max_num)).sort
     #元の配列から、抽出済みの要素を削除する
     all_people_array = all_people_array - new_groups[-1]
+  else
+    new_groups << all_people_array
   end
+  }
   return new_groups
 end
 
-peoples = ('A'..'F').to_a
+def separete_group2
 
-g_nums = num_of_people_in_group(peoples.length,2)
-separete_group(g_nums,peoples).map do |group|
+end
+
+peoples = ('A'..'F').to_a
+separete_group(peoples,2).map do |group|
   p(group)
 end
