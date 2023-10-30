a3966fed4cf069150511cc76aee8925120a0ce5e
8t104k <8t104k@gmail.com>
2023-10-30 23:12:23 +0900

    引数に対する処理を統合

--- calender/calendar.rb
+++ calender/calendar.rb
@@ -2,26 +2,30 @@ require 'Date'
 require 'optparse'
 
 params = ARGV.getopts("m:")
+
 if params['m'] == nil
-  m = Date.today.strftime('%m').to_i
+  #引数がなければ時効した日付の月を取得する
+  arg_m = Date.today.strftime('%m').to_i
+elsif params['m'] >= 1 && params['m'] <= 12
+  arg_m = params['m'].to_i
+elsif params['m'] < 1 || params['m'] > 12
+  #1以下または12以上の場合はエラーを返す
+  raise "#{arg_m} is neither a month number (1..12) nor a name"
 else
-  m = params['m'].to_i
+  #それ以外の文字列はエラーを出す
+  raise "#{arg_m} is not a suitable argument"
 end
 
-if m >= 1 && m <= 12
-  s_date = Date.new(2023,m,1)
-  e_date = Date.new(2023,m,-1)
-else
-  raise "#{m} is neither a month number (1..12) nor a name"
-end
-
+s_date = Date.new(2023,arg_m,1)
+e_date = Date.new(2023,arg_m,-1)
 month = "    #{s_date.strftime('%B')} #{s_date.strftime('%Y')}"
 weeks = 'Mo Tu We Th Fr Sa Su '
 days = (1..e_date.strftime("%e").to_i).to_a
 
-#からの配列ぶち込む
+#配列daysに、1日の開始曜日分、空の配列ぶち込む
 days = Array.new(((s_date.wday.to_i)+6)%7,'') + days
 
+#カレンダーの表示
 puts(month + "\n")
 puts(weeks + "\n")
 days.each.with_index(1) { |day,i|
@@ -30,4 +34,3 @@ if i%7 == 0
   print("\n")
 end
 }
-print("\n")
