951417ab61e472f5a1d0032d2c70b3ec8ce959cd
8t104k <8t104k@gmail.com>
2023-10-30 22:46:02 +0900

    warningが出ないように対応

--- calender/calendar.rb
+++ calender/calendar.rb
@@ -1,35 +1,29 @@
 require 'Date'
 require 'optparse'
 
-opt = OptionParser.new
-
-opt.on('-m') {|v| p v }
-argv = opt.parse(ARGV)
-p(argv)
-
-#引数のバリデーション
-if argv == []
+params = ARGV.getopts("m:")
+if params['m'] == nil
   m = Date.today.strftime('%m').to_i
 else
-  return true if argv[0].to_i > 12
-  m = argv[0].to_i
+  m = params['m'].to_i
 end
 
-s_date = Date.new(2023,m,1)
-e_date = Date.new(2023,m,-1)
+if m >= 1 && m <= 12
+  s_date = Date.new(2023,m,1)
+  e_date = Date.new(2023,m,-1)
+else
+  raise "#{m} is neither a month number (1..12) nor a name"
+end
+
 month = "    #{s_date.strftime('%B')} #{s_date.strftime('%Y')}"
 weeks = 'Mo Tu We Th Fr Sa Su '
 days = (1..e_date.strftime("%e").to_i).to_a
-#p(s_date.strftime("%a"))
-#p(((s_date.wday.to_i)+6)%7)
+
 #からの配列ぶち込む
 days = Array.new(((s_date.wday.to_i)+6)%7,'') + days
 
-printf(month)
-print("\n")
-printf(weeks)
-print("\n")
-
+puts(month + "\n")
+puts(weeks + "\n")
 days.each.with_index(1) { |day,i|
 printf("%3s",day.to_s + " ")
 if i%7 == 0
