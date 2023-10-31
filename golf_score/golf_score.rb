37ad95913dca7d7d7d7c0dc02c923736daf6f532
8t104k <8t104k@gmail.com>
2023-10-31 22:10:58 +0900

    STDINの受け取り方を変更

--- golf_score/golf_score.rb
+++ golf_score/golf_score.rb
@@ -1,17 +1,5 @@
-hash_value = Hash.new
-hash_key = [:par,:play]
-#lines = [[4,4,5,3,5,4,4,3,4,4,5,4,4,3,4,4,3,5],[2,3,1,5,8,3,5,1,5,6,2,5,7,2,5,5,2,6]]
-
-
-lines = readlines(chomp: true).map{|line| line.split(',').map(&:to_i)}
-#lines = std_in.map{|line| line.split(',').map(&:to_i)}
-#lines = readlines(chomp: true).map.with_index{|line,i| lines.store(hash_value[i],line.split(',').map(&:to_i))}
-#lines = readlines(chomp: true).map.with_index{|line,i| lines.store(hash_value[i],line)}
-#ary = [hash_key,lines].transpose
-#h = Hash[*ary]
-#h = [hash_key,lines].transpose.to_h
-pars = lines[0]
-scores = lines[1]
+pars = gets.split(',').map(&:to_i)
+scores = gets.split(',').map(&:to_i)
 
 def output_score(par_num,play_num)
   score = play_num - par_num
