e67651f7313032c3dcf4a9a7270eb69e10bb2a8a
8t104k <8t104k@gmail.com>
2023-11-03 00:17:56 +0900

    VendingMachineのクラスに関数を追加

--- vending_machine/vending_machine.rb
+++ vending_machine/vending_machine.rb
@@ -4,32 +4,50 @@ class Suica
   end
   def charge(charge_amount)
     if charge_amount < 100
-      return false
+      false
     else
       @balance += charge_amount
+      true
     end
   end
 
-  def show
-    return @balance
+  def amount
+    @balance
   end
 end
 
 class VenderMachine
+  PRODUCTS = ['ペプシ']
+  PRICE = [150]
   def initialize
-    {name: 'ペプシ', price: 150}
+    @stocks = {0 => 5}
+  end
+
+  def check_stock(product)
+    @stocks[0]
+  end
+  def able_to_buy?(suica,juice)
+    if suica.amount > juice.price && @stocks[0] > 0
+      true
+    else
+      false
+    end
+
   end
 end
 
 class Juice
-  def initialize
-
+  def initialize(name, price)
+    @name = name
+    @price = price
   end
 end
 
 
-
+test_vender1 = VenderMachine.new()
 test1 = Suica.new()
-puts test1.show
+
+puts test1.amount
 puts test1.charge(300)
-puts test1.show
+puts test1.amount
+puts test_vender1.check_stock()
