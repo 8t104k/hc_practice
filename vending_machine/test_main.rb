d48cba4f2cae4a86526705c63a3000e324cbe55c
8t104k <8t104k@gmail.com>
2023-11-05 00:19:16 +0900

    ファイル名を変更

---
+++ vending_machine/test_main.rb
@@ -0,0 +1,26 @@
+test_vender1 = VenderMachine.new()
+test1 = Suica.new()
+
+
+puts test1.amount
+puts test1.charge(300)
+puts test1.amount
+puts test_vender1.check_stock('ペプシ')
+puts test_vender1.able_to_buy?(test1,'ペプシ')
+puts test_vender1.buy(test1,'ペプシ')
+puts test_vender1.buy(test1,'いろはす')
+puts test_vender1.buy(test1,'モンスター')
+puts test_vender1.buy(test1,'モンスター')
+puts test_vender1.buy(test1,'モンスター')
+puts test1.charge(10000)
+puts test_vender1.buy(test1,'モンスター')
+puts test_vender1.buy(test1,'モンスター')
+puts test_vender1.buy(test1,'モンスター')
+p test_vender1.show_buyable_list
+puts test_vender1.buy(test1,'モンスター')
+puts test_vender1.check_stock('モンスター')
+puts test_vender1.fill_products('モンスター',10)
+puts test_vender1.check_stock('モンスター')
+puts test1.amount
+puts test_vender1.show_sales
+p test_vender1.show_buyable_list
