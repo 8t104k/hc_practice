8804e17691b01a967106f28277cac49681134cc2
8t104k <8t104k@gmail.com>
2023-11-05 01:34:38 +0900

    在庫を配列としてではなく、Juiceのインスタンスを持つように変更

--- vending_machine/vendingmachine.rb
+++ vending_machine/vendingmachine.rb
@@ -1,22 +1,28 @@
 class VendingMachine
   PRODUCTS = {
-    'ペプシ' => {price: 150, stock: 5},
-    'モンスター' => {price:230, stock: 5},
-    'いろはす' => {price:120, stock: 5}
+    'ペプシ' => {price: 150},
+    'モンスター' => {price:230},
+    'いろはす' => {price:120}
   }
+  INITIAL_STOCK = 5
 
   def initialize
     @sales = 0
-    @products = PRODUCTS
+    @products = {}
+    PRODUCTS.map do |product_name,info|
+      @products[product_name] = []
+      #初期ストック分、Juiceのインスタンスを作成
+      INITIAL_STOCK.times{ @products[product_name] << Juice.new(product_name,info[:price])}
+    end
   end
 
   def check_stock(product)
-    return @products[product][:stock] if @products.key?(product)
+    return @products[product].length if @products.key?(product)
   end
   def able_to_buy?(suica,product)
     if @products.key?(product)
-      price = @products[product][:price]
-      stock = @products[product][:stock]
+      price = PRODUCTS[product][:price]
+      stock = @products[product].length
       suica.amount > price && stock > 0
     else
       false
@@ -24,15 +30,16 @@ class VendingMachine
   end
   def buy(suica,product)
     amount = suica.amount
-    price = @products[product][:price]
-    stock = @products[product][:stock]
+    price = PRODUCTS[product][:price]
+    stock = @products[product].length
 
     return '残高が足りません' if amount < price
     return '在庫がありません' if stock <= 0
     @sales += price
-    @products[product][:stock] -= 1
+    #@products[product][:stock] -= 1
     suica.buy(price)
-    return Juice.new(product,price)
+    return @products[product][0]
+    @products[product].delete_at(0)
   end
 
   def show_sales
@@ -40,13 +47,14 @@ class VendingMachine
   end
 
   def show_buyable_list
-    buyable_list = @products.select { |product,p_info| p_info[:stock] > 0 }.keys
+    buyable_list = @products.select { |product,p_info| p_info.length > 0 }.keys
     #buyable_list = buyable_list.keys
   end
 
-  def fill_products(product, fill_num)
-    if @products.key?(product)
-      @products[product][:stock] += fill_num
+  def fill_products(product_name, fill_num)
+    if @products.key?(product_name)
+      price = PRODUCTS[product_name][:price]
+      @products[product_name] << Juice.new(product_name,price)
     end
   end
 
