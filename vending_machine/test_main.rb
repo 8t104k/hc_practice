b1db0bb584ba539640392524bd240d8676ba1f58
8t104k <8t104k@gmail.com>
2023-11-05 13:10:24 +0900

    attr_readerに対応

--- vending_machine/test_main.rb
+++ vending_machine/test_main.rb
@@ -9,7 +9,7 @@ puts test1.amount
 #エラーになるでOK
 #puts test1.amount = 1000
 puts test1.charge(300)
-puts test1.amount
+puts test1.balance
 puts test_vender1.check_stock('ペプシ')
 puts test_vender1.able_to_buy?(test1,'ペプシ')
 puts test_vender1.buy(test1,'ペプシ')
@@ -26,6 +26,6 @@ puts test_vender1.buy(test1,'モンスター')
 puts test_vender1.check_stock('モンスター')
 puts test_vender1.fill_products('モンスター')
 puts test_vender1.check_stock('モンスター')
-puts test1.amount
+puts test1.balance
 puts test_vender1.show_sales
 p test_vender1.show_buyable_list
