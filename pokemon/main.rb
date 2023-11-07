09c28f45ad64d372281ab395935a72c9ac9214f1
8t104k <8t104k@gmail.com>
2023-11-07 23:18:29 +0900

    NameServiseモジュールを修正

--- pokemon/main.rb
+++ pokemon/main.rb
@@ -1,9 +1,9 @@
 module NameService
-  def chage_name(new_name)
-    if new_name == 'うんこ'
-      '不適切な名前です'
-    end
-    @name = new_name
+  def valid_name_is?(new_name)
+    new_name != 'うんこ'
+  end
+  def get_name
+    @name
   end
 end
 
@@ -21,6 +21,28 @@ class Pokemon
     "#{@name} のこうげき!"
   end
 
+  def pokemon_name_change(new_name)
+    if valid_name_is?(new_name)
+      @name = new_name
+    else
+      '不適切な名前です'
+    end
+  end
+
+end
+
+class Player
+  include NameService
+  def initialize(name)
+    @name = name
+  end
+  def user_name_change(new_name)
+    if valid_name_is?(new_name)
+      @name = new_name
+    else
+      '不適切な名前です'
+    end
+  end
 end
 
 
@@ -41,5 +63,11 @@ end
 #puts poke.attack
 
 pika = Pikachu.new("ピカチュウ", "でんき", "", 100)
+p1 = Player.new('ワシ')
 puts pika.name
 puts pika.attack
+puts p1.get_name
+puts p1.user_name_change('A')
+puts p1.get_name
+puts p1.user_name_change('うんこ')
+puts p1.get_name
